fb686e6e1e12809a231fa8a440308576b64124cb
kasimov/ <kasimov.mv@mail.ru>
2018-11-22 08:48:15 +0400

    переход между окнами

--- app/src/main/java/com/example/v_s/travers_v1/etapActivity.java
+++ app/src/main/java/com/example/v_s/travers_v1/etapActivity.java
@@ -1,7 +1,9 @@
 package com.example.v_s.travers_v1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class etapActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class etapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_etap);
+    }
+
+    public void spisok_shtraf(View view) {
+        Intent intObj = new Intent(this, spisokShtrafActivity.class);
+        startActivity(intObj);
+
     }
 }
