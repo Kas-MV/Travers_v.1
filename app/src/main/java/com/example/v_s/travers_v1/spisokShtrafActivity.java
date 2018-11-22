8fc5d9f94789c3d8ecbe3cc33b7b9b6d39feee15
kasimov/ <kasimov.mv@mail.ru>
2018-11-22 14:32:01 +0400

    переход между окнами

--- app/src/main/java/com/example/v_s/travers_v1/spisokShtrafActivity.java
+++ app/src/main/java/com/example/v_s/travers_v1/spisokShtrafActivity.java
@@ -1,7 +1,9 @@
 package com.example.v_s.travers_v1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class spisokShtrafActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class spisokShtrafActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spisok_shtraf);
+    }
+
+    public void shtrafScreen(View view) {
+        Intent intObj = new Intent(this, shtrafActivity.class);
+        startActivity(intObj);
+
     }
 }
